2e289db99d4cefe06fe265f85071232200f65471
Shreyasd10 <52241691+Shreyasd10@users.noreply.github.com>
2020-08-07 11:24:22 +0530

    Updates

---
+++ precache-manifest.ec946edfcbbd295788d3e26a147dbabc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f9973eef9e7acbcfafb3a5c107097f",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d65375604815ccdeb2df",
+    "url": "/Robots/static/css/main.63c00763.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robots/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65375604815ccdeb2df",
+    "url": "/Robots/static/js/main.53748b95.chunk.js"
+  },
+  {
+    "revision": "f756081b821c21819cc9",
+    "url": "/Robots/static/js/runtime-main.a416d013.js"
+  },
+  {
+    "revision": "04ba6ae2bc75a49d5ee8c473f1d7c2fa",
+    "url": "/Robots/static/media/heatwave-webfont.04ba6ae2.woff2"
+  },
+  {
+    "revision": "4fffd163602a3f38311f4f855630856c",
+    "url": "/Robots/static/media/heatwave-webfont.4fffd163.woff"
+  }
+]);
